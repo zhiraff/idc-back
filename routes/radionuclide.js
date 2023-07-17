193a0b47b49c29fdbdaf47e376c0a50006b91371
Tima Pigolev <timapigolev@gmail.com>
2023-07-17 17:03:07 +0700

    fix/auth/login

--- routes/radionuclide.js
+++ routes/radionuclide.js
@@ -1,128 +1,128 @@
-/**
- * @swagger
- * components:
- *   schemas:
- *     Radionuclides:
- *       type: object
- *       required:
- *         - name
- *         - name_simple
- *       properties:
- *         id:
- *           type: integer
- *           description: id радионуклида
- *         name:
- *           type: string
- *           description: название радионуклида
- *         name_simple:
- *           type: string
- *           description: Упрощённое обозначение радионуклида
- *         name_beauty:
- *           type: string
- *           description: html-код для вставки названия
- *       example:
- *         id: 1
- *         name: Уран 234
- *         name_simple: U234
- *         name_beauty: U<sup>238</sup>
- */
-/**
- * @swagger
- * tags:
- *   name: Radionuclides
- *   description: API для работы со справочником перечень радионуклидов
- * /Radionuclides:
- *   get:
- *     summary: Просмотр всех радионуклидов
- *     tags: [Radionuclides]
- *     responses:
- *       200:
- *         description: Просмотр всех радионуклидов
- *         content:
- *           application/json:
- *             schema:
- *               type: array
- *               items:
- *                 $ref: '#/components/schemas/Radionuclides'
- *   post:
- *     summary: Создание нового радионуклида
- *     tags: [Radionuclides]
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             $ref: '#/components/schemas/Radionuclides'
- *     responses:
- *       200:
- *         description: Радионуклид добавлен.
- *         content:
- *           application/json:
- *             schema:
- *               $ref: '#/components/schemas/Radionuclides'
- *       500:
- *         description: Ошибка сервера
- * /Radionuclides/{id}:
- *   get:
- *     summary: Просмотр радионуклида по id
- *     tags: [Radionuclides]
- *     parameters:
- *       - in: path
- *         name: id
- *         schema:
- *           type: string
- *         required: true
- *         description: id радионуклида
- *     responses:
- *       200:
- *         description: радионуклид найденный по id
- *         contens:
- *           application/json:
- *             schema:
- *               $ref: '#/components/schemas/Radionuclides'
- *       404:
- *         description: радионуклид не найден
- *   put:
- *    summary: Обновление радионуклида по id
- *    tags: [Radionuclides]
- *    parameters:
- *      - in: path
- *        name: id
- *        schema:
- *          type: string
- *        required: true
- *        description: id радионуклида
- *    requestBody:
- *      required: true
- *      content:
- *        application/json:
- *          schema:
- *            $ref: '#/components/schemas/Radionuclides'
- *    responses:
- *      200:
- *        description: радионуклид обновлён
- *        content:
- *          application/json:
- *            schema:
- *              $ref: '#/components/schemas/Radionuclides'
- *      404:
- *        description: радионуклид не найден
- *      500:
- *        description: Ошибка сервера
- *   delete:
- *     summary: Удаление радионуклида по id
- *     tags: [Radionuclides]
- *     parameters:
- *       - in: path
- *         name: id
- *         schema:
- *           type: string
- *         required: true
- *         description: id радионуклида
- *
- *     responses:
- *       200:
- *         description: радионуклид удалён
- *       404:
- *         description: радионуклид не найден
- */
+/*  
+   @swagger
+   components:
+     schemas:
+       Radionuclides:
+         type: object
+         required:
+           - name
+           - name_simple
+         properties:
+           id:
+             type: integer
+             description: id радионуклида
+           name:
+             type: string
+             description: название радионуклида
+           name_simple:
+             type: string
+             description: Упрощённое обозначение радионуклида
+           name_beauty:
+             type: string
+             description: html-код для вставки названия
+         example:
+           id: 1
+           name: Уран 234
+           name_simple: U234
+           name_beauty: U<sup>238</sup>
+  */
+/*  
+   @swagger
+   tags:
+     name: Radionuclides
+     description: API для работы со справочником перечень радионуклидов
+   /Radionuclides:
+     get:
+       summary: Просмотр всех радионуклидов
+       tags: [Radionuclides]
+       responses:
+         200:
+           description: Просмотр всех радионуклидов
+           content:
+             application/json:
+               schema:
+                 type: array
+                 items:
+                   $ref: '#/components/schemas/Radionuclides'
+     post:
+       summary: Создание нового радионуклида
+       tags: [Radionuclides]
+       requestBody:
+         required: true
+         content:
+           application/json:
+             schema:
+               $ref: '#/components/schemas/Radionuclides'
+       responses:
+         200:
+           description: Радионуклид добавлен.
+           content:
+             application/json:
+               schema:
+                 $ref: '#/components/schemas/Radionuclides'
+         500:
+           description: Ошибка сервера
+   /Radionuclides/{id}:
+     get:
+       summary: Просмотр радионуклида по id
+       tags: [Radionuclides]
+       parameters:
+         - in: path
+           name: id
+           schema:
+             type: string
+           required: true
+           description: id радионуклида
+       responses:
+         200:
+           description: радионуклид найденный по id
+           contens:
+             application/json:
+               schema:
+                 $ref: '#/components/schemas/Radionuclides'
+         404:
+           description: радионуклид не найден
+     put:
+      summary: Обновление радионуклида по id
+      tags: [Radionuclides]
+      parameters:
+        - in: path
+          name: id
+          schema:
+            type: string
+          required: true
+          description: id радионуклида
+      requestBody:
+        required: true
+        content:
+          application/json:
+            schema:
+              $ref: '#/components/schemas/Radionuclides'
+      responses:
+        200:
+          description: радионуклид обновлён
+          content:
+            application/json:
+              schema:
+                $ref: '#/components/schemas/Radionuclides'
+        404:
+          description: радионуклид не найден
+        500:
+          description: Ошибка сервера
+     delete:
+       summary: Удаление радионуклида по id
+       tags: [Radionuclides]
+       parameters:
+         - in: path
+           name: id
+           schema:
+             type: string
+           required: true
+           description: id радионуклида
+  
+       responses:
+         200:
+           description: радионуклид удалён
+         404:
+           description: радионуклид не найден
+  */
--- routes/user.js
+++ routes/user.js
@@ -1,145 +1,145 @@
-/**
- * @swagger
- * components:
- *   schemas:
- *     Users:
- *       type: object
- *       required:
- *         - username
- *         - password
- *       properties:
- *         id:
- *           type: integer
- *           description: id пользователя
- *         username:
- *           type: string
- *           description: имя пользователя
- *         firstName:
- *           type: string
- *           description: Имя
- *         secondName:
- *           type: string
- *           description: Фамилия
- *         thirdName:
- *           type: string
- *           description: Отчество
- *         role:
- *           type: string
- *           description: Роль
- *       example:
- *         id: 1
- *         username: admin
- *         firstName: Тимофей
- *         secondName: Пиголев
- *         thirdName: Валерьевич
- *         role: administrator
- *   securitySchemes:
- *     cookieAuth:
- *       type: apiKey
- *       in: cookie
- *       name: JSESSIONID
- * security:
- *   - cookieAuth: []
- */
-/**
- * @swagger
- * tags:
- *   name: Users
- *   description: API для работы с пользователями АС
- * /users:
- *   get:
- *     security:
- *       - cookieAuth: []
- *     summary: Просмотр всех пользователей
- *     tags: [Users]
- *     responses:
- *       200:
- *         description: Просмотр всех пользователей
- *         content:
- *           application/json:
- *             schema:
- *               type: array
- *               items:
- *                 $ref: '#/components/schemas/Users'
- *   post:
- *     summary: Создание нового пользователя
- *     tags: [Users]
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             $ref: '#/components/schemas/Users'
- *     responses:
- *       200:
- *         description: id созданного пользователя
- *         content:
- *           application/json:
- *             schema:
- *               $ref: '#/components/schemas/Users'
- *       500:
- *         description: ошибка сервера
- * /users/{id}:
- *   get:
- *     summary: Просмотр пользователя по id
- *     tags: [Users]
- *     parameters:
- *       - in: path
- *         name: id
- *         schema:
- *           type: string
- *         required: true
- *         description: id пользователя
- *     responses:
- *       200:
- *         description: Пользователь найденный по id
- *         contens:
- *           application/json:
- *             schema:
- *               $ref: '#/components/schemas/Users'
- *       404:
- *         description: Пользователь не найден
- *   put:
- *    summary: Обновление пользователя по id
- *    tags: [Users]
- *    parameters:
- *      - in: path
- *        name: id
- *        schema:
- *          type: string
- *        required: true
- *        description: id пользователя
- *    requestBody:
- *      required: true
- *      content:
- *        application/json:
- *          schema:
- *            $ref: '#/components/schemas/Users'
- *    responses:
- *      200:
- *        description: Пользователб обновлён
- *        content:
- *          application/json:
- *            schema:
- *              $ref: '#/components/schemas/Users'
- *      404:
- *        description: Пользователь не найден
- *      500:
- *        description: Ошибка сервера
- *   delete:
- *     summary: Удаление пользователя по id
- *     tags: [Users]
- *     parameters:
- *       - in: path
- *         name: id
- *         schema:
- *           type: string
- *         required: true
- *         description: id пользователя
- *
- *     responses:
- *       200:
- *         description: Пользователь удалён
- *       404:
- *         description: Пользователь не найден
- */
+/*  
+   @swagger
+   components:
+     schemas:
+       Users:
+         type: object
+         required:
+           - username
+           - password
+         properties:
+           id:
+             type: integer
+             description: id пользователя
+           username:
+             type: string
+             description: имя пользователя
+           firstName:
+             type: string
+             description: Имя
+           secondName:
+             type: string
+             description: Фамилия
+           thirdName:
+             type: string
+             description: Отчество
+           role:
+             type: string
+             description: Роль
+         example:
+           id: 1
+           username: admin
+           firstName: Тимофей
+           secondName: Пиголев
+           thirdName: Валерьевич
+           role: administrator
+     securitySchemes:
+       cookieAuth:
+         type: apiKey
+         in: cookie
+         name: JSESSIONID
+   security:
+     - cookieAuth: []
+  */
+/*  
+   @swagger
+   tags:
+     name: Users
+     description: API для работы с пользователями АС
+   /users:
+     get:
+       security:
+         - cookieAuth: []
+       summary: Просмотр всех пользователей
+       tags: [Users]
+       responses:
+         200:
+           description: Просмотр всех пользователей
+           content:
+             application/json:
+               schema:
+                 type: array
+                 items:
+                   $ref: '#/components/schemas/Users'
+     post:
+       summary: Создание нового пользователя
+       tags: [Users]
+       requestBody:
+         required: true
+         content:
+           application/json:
+             schema:
+               $ref: '#/components/schemas/Users'
+       responses:
+         200:
+           description: id созданного пользователя
+           content:
+             application/json:
+               schema:
+                 $ref: '#/components/schemas/Users'
+         500:
+           description: ошибка сервера
+   /users/{id}:
+     get:
+       summary: Просмотр пользователя по id
+       tags: [Users]
+       parameters:
+         - in: path
+           name: id
+           schema:
+             type: string
+           required: true
+           description: id пользователя
+       responses:
+         200:
+           description: Пользователь найденный по id
+           contens:
+             application/json:
+               schema:
+                 $ref: '#/components/schemas/Users'
+         404:
+           description: Пользователь не найден
+     put:
+      summary: Обновление пользователя по id
+      tags: [Users]
+      parameters:
+        - in: path
+          name: id
+          schema:
+            type: string
+          required: true
+          description: id пользователя
+      requestBody:
+        required: true
+        content:
+          application/json:
+            schema:
+              $ref: '#/components/schemas/Users'
+      responses:
+        200:
+          description: Пользователб обновлён
+          content:
+            application/json:
+              schema:
+                $ref: '#/components/schemas/Users'
+        404:
+          description: Пользователь не найден
+        500:
+          description: Ошибка сервера
+     delete:
+       summary: Удаление пользователя по id
+       tags: [Users]
+       parameters:
+         - in: path
+           name: id
+           schema:
+             type: string
+           required: true
+           description: id пользователя
+  
+       responses:
+         200:
+           description: Пользователь удалён
+         404:
+           description: Пользователь не найден
+  */
